23a0b97b5ad5c895237a71bcb808161b056495a3
Clyde Caradine <caradi_c@epita.fr>
2019-10-23 19:56:52 +0200

    [LIST] fixed some tests and add some todo.

--- sequential_structures/list/list.cpp
+++ sequential_structures/list/list.cpp
@@ -3,32 +3,49 @@
 
 #include<iostream>
 #include<stdexcept>
+#include<cstring>
+#define CAP_INIT_SIZE 8
 
 class List {
 	int size_ = 0;
-	int cap_ = 8;
+	int cap_ = CAP_INIT_SIZE;
 	int *array_;
+
+  int* create_and_init_buffer() {
+    int* tmp = new int[this->cap_];
+    for (std::size_t i = 0; i < (unsigned)this->cap_; ++i)
+      tmp[i] = 0;
+    return tmp;
+  };
  
 public:
-	List(): size_(0), cap_(8), array_(new int[this->cap_]) {};
+  List(): size_(0), cap_(8), array_(create_and_init_buffer()) {};
 
 	int len() const {
-		return size_;
+		return this->size_;
 	};
 
-	int at(int i) {
+	int at(int i) const {
+	  // TODO: Throw empty list exception.
 		if (i > this->size_)
 			throw std::out_of_range("Cannot acces out of bound value.\n");
 		return this->array_[i];
 	};
 
-	int operator[](int i) {
+	int operator[](int i) const {
 		return this->at(i);
 	}
 
 	void pushback(int n) {
 		this->array_[this->size_] = n;
 		this->size_++;
+		if (this->size_ == this->cap_) {
+		  int* tmp = new int[this->cap_ * 2];
+		  std::memcpy(tmp, this->array_, this->cap_ * sizeof(int));
+		  this->cap_ *= 2;
+		  delete[] this->array_;
+		  this->array_ = tmp;
+		}  
 
 	};
 
@@ -36,6 +53,18 @@ public:
 		this->size_--;
 	};
 };
+
+TEST_CASE("List initialization", "[LIST-INIT]") {
+	List l{};
+
+	for (std::size_t i = 0; i < CAP_INIT_SIZE; ++i)
+	  REQUIRE( l[i] == 0 );
+	REQUIRE( l.len() == 0 );	
+	REQUIRE( l.at(0) == 0 );
+	REQUIRE_THROWS_AS( l.at(12), std::out_of_range );
+	REQUIRE( l[0] == 0 );
+	REQUIRE_THROWS_AS( l[12], std::out_of_range );
+}
 
 TEST_CASE("Basic List tests", "[LIST]") {
 	List l{};
@@ -47,17 +76,6 @@ TEST_CASE("Basic List tests", "[LIST]") {
 
 	l.remove(0);
 	REQUIRE( l.len() == 0 );
-
-}
-
-TEST_CASE("List creation", "[LIST-CREATION]") {
-	List l{};
-
-	REQUIRE( l.len() == 0 );	
-	REQUIRE( l.at(0) == 0 );
-	REQUIRE_THROWS_AS( l.at(12), std::out_of_range );
-	REQUIRE( l[0] == 0 );
-	REQUIRE_THROWS_AS( l[12], std::out_of_range );
 }
 
 TEST_CASE("Pushback", "[LIST-PUSHBACK]") {
