6e025a2ebabc66780a2295f265cf702e97ba774b
mortician <mortician@morticians-MacBook-Air.local>
2020-10-07 13:35:04 +0200

    [TODO] Create todo.

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY . /usr/src/c--data-structure
 
 WORKDIR /usr/src/c--data-structure/sequential_structures/list
 
- RUN make all
+RUN make all
+RUN make test
--- sequential_structures/list/list.hh
+++ sequential_structures/list/list.hh
@@ -1,29 +1,32 @@
-#include<iostream>
-#include<stdexcept>
-#include<cstring>
+#include <iostream>
+#include <stdexcept>
+#include <cstring>
 
 #include "exception.hh"
 
 #define CAP_INIT_SIZE 8
 
-class List {
+class List
+{
   int size_ = 0;
   int cap_ = CAP_INIT_SIZE;
   int *array_;
 
-  int* create_and_init_buffer();
- 
-public:
-  List(): size_(0), cap_(8), array_(create_and_init_buffer()) {};
+  int *create_and_init_buffer();
 
-  inline int operator[](int i) const {
+public:
+  List() : size_(0), cap_(8), array_(create_and_init_buffer()){};
+
+  inline int operator[](int i) const
+  {
     return this->at(i);
   }
-  
-  inline int len() const {
+
+  inline int len() const
+  {
     return this->size_;
   };
-  
+
   int at(int i) const;
   void pushback(int n);
   void remove(int i);
